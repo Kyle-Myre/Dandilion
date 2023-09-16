64f7ec56d00391401649d41c58a1db2d6427c606
Sikrox Memer <simoarmymen9@gmail.com>
2023-09-16 22:31:42 +0100

    Added App Class (main class)

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import customtkinter as ct
+from tkinter import StringVar
+
+
+class App(ct.CTk):
+
+    ct.set_appearance_mode("System")
+    ct.set_default_color_theme("green")
+
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("480x480")
+
+        self.Entry_Label = ct.CTkLabel(self , text="Put a Video Link")
+        self.Entry_Label.pack(padx=5 , pady=5)
+
+        self.value = StringVar()
+
+        self.Entry = ct.CTkEntry(self , width=370 , height=25 , textvariable=self.value)
+        self.Entry.pack(padx=5 , pady=5)
+
+        self.__Types = ['MP3' , 'MP4']
+        self.options = StringVar(value=self.__Types[0])
+
+        self.Type = ct.CTkOptionMenu(self , variable=self.options , values=self.__Types)
+        self.Type.pack(padx=5 , pady=5)
+
+        self.Download_Button = ct.CTkButton(self , text="Download" , command=lambda:print(self.options.get()))
+        self.Download_Button.pack(padx=5 , pady=5)
+
+        self.mainloop()
+
+
+App()

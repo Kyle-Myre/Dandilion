7718032bd34542a3cd0dfbb54e5bd5c37a1c2286
Sikrox Memer <simoarmymen9@gmail.com>
2023-09-16 23:47:20 +0100

    Added Convertor & Yt_Options Class

--- src/main.py
+++ src/main.py
@@ -2,8 +2,33 @@ from tkinter import StringVar
 from pytube import YouTube 
 import customtkinter as ct
 
+class Y_Options:
+    
+    def __init__(self ,  output:str) -> None:
+        self.output = output
 
+    def Give(self):
+        return {
+            "format" : "bestaudio" ,
+            "outtmpl" : self.output ,
+            "noplaylist" : True ,
+            'continue_dl': True,
+            'postprocessors': [ 
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'wav',
+                    'preferredquality': '192', 
+                }
+            ]
 
+        }   
+
+class Convertor:
+    @staticmethod
+    def Download(link:str , options:dict=None):
+        youtube_obj = YouTube(link)
+        youtube_obj.streams.first().download("./output")
+        
 class App(ct.CTk):
 
     _SETTINGS = {
@@ -32,7 +57,7 @@ class App(ct.CTk):
         self.Type = ct.CTkOptionMenu(self , variable=self.options , values=self.__Types)
         self.Type.pack(padx=5 , pady=5)
 
-        self.Download_Button = ct.CTkButton(self , text="Download" , command=lambda:print(self.options.get()))
+        self.Download_Button = ct.CTkButton(self , text="Download" , command=lambda:Convertor.Download(self.value.get()))
         self.Download_Button.pack(padx=5 , pady=5)
 
         self.Progress = "";

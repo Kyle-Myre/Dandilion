8bf572cf578b4c7105a0bb71189485167c7d0100
SikroxMemer <simoarmymen9@gmail.com>
2023-11-03 10:30:52 +0100

    ground zero

---
+++ Client/src/components/Form.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class Form extends Component {
+    constructor (props) {
+        super(props)
+    }
+
+    while_request() {
+
+    }
+
+    request() {
+
+    }
+
+    render() {
+        return <form onSubmit={e => e.preventDefault}>
+            
+        </form>
+    }
+}

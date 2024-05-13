bbf167056bea16041734b310c28b34718a733357
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-05-13 15:36:52 +0100

    Added Progress bar, Minor Changes

--- src/main.py
+++ src/main.py
@@ -1,43 +1,38 @@
 # imports
 try:
-    from tkinter import StringVar
+    import pytube.request
+    import tkinter as tk
     from threading import Thread
     from pytube import YouTube
-    import pytube.request
-    import os
-    import time
+    from os import path, rename
     from datetime import datetime
     from rich.console import Console
-    import darkdetect
-    import tkinter as tk
+    from tkinter import StringVar
     from tkinter import ttk
 
 except (ImportError, ImportWarning) as err:
     print(f"Missing Library Error : {err}")
 
-pytube.request.default_range_size = 9437184  # MB
+pytube.request.default_range_size = 10000  # 10 GB
 console = Console()
 
 App = tk.Tk()
 
 icon_relative_path = ""
 
-icon_relative_path = os.path.join(os.path.dirname(__file__), 'assets', 'public', 'Icon.ico')
-
+icon_relative_path = path.join(path.dirname(__file__), 'assets', 'public', 'Icon.ico')
 
 App.iconbitmap(icon_relative_path)
 
-theme_relative_path = os.path.join(os.path.dirname(__file__), 'assets', 'themes', 'forest-dark.tcl')
+theme_relative_path = path.join(path.dirname(__file__), 'assets', 'themes', 'forest-dark.tcl')
 App.tk.call('source', theme_relative_path)
 ttk.Style().theme_use('forest-dark')
 
 def Download(link: str):
     FinishLabel.configure(text="Fetching . . .", foreground="#FFFF00")
-    time.sleep(3)
 
     if link == "":
         FinishLabel.configure(text="Empty Link")
-        time.sleep(2)
         FinishLabel.configure(text="")
 
         return None
@@ -47,7 +42,6 @@ def Download(link: str):
 
     if options.get() == "MP4":
         try:
-
             video = Y_obj.streams.get_highest_resolution()
             VideoTitle.configure(text=video.title)
 
@@ -56,17 +50,13 @@ def Download(link: str):
             video.download("./output")
 
             FinishLabel.configure(text="Finished", foreground="green")
-            time.sleep(5)
             FinishLabel.configure(text="")
 
         except Exception as error:
-
             FinishLabel.configure(text=error, foreground="red")
 
     elif options.get() == "MP3":
-
         try:
-
             video = Y_obj.streams.filter(only_audio=True).first()
 
             FinishLabel.configure(text="Video founded")
@@ -74,29 +64,25 @@ def Download(link: str):
             VideoTitle.configure(text=video.title)
             output_file = video.download("./output")
 
-            name, ext = os.path.splitext(output_file)
+            name, _ = path.splitext(output_file)
             new_file = name + ".mp3"
 
-            os.rename(output_file, new_file)
+            rename(output_file, new_file)
 
             FinishLabel.configure(text="Finished", foreground="green")
-            time.sleep(5)
             FinishLabel.configure(text="")
 
         except Exception as error:
-
             FinishLabel.configure(text=error, foreground="red")
 
-
-def on_progress(stream, chunk, bytes_remaining):
+def on_progress(stream, _, bytes_remaining):
     """Callback function"""
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     pct_completed = bytes_downloaded / total_size * 100
 
-    Progress.configure(text=f"{str(round(pct_completed))}%")
-    Progress.update()
-
+    Progress['value'] = pct_completed
+    Progress_Label.configure(text=f"{int(pct_completed)}%")
 
 # UI
 __Types = ["", "MP3", "MP4"]
@@ -130,18 +116,18 @@ Download_Button.pack(padx=5, pady=5)
 VideoTitle = ttk.Label(App, text="")
 VideoTitle.pack()
 
-Progress = ttk.Label(App, text="0%")
-Progress.pack()
+Progress = ttk.Progressbar(App, orient="horizontal", length=300, mode="determinate")
+Progress.pack(padx=5, pady=5)
 
+Progress_Label = ttk.Label(App, text="")
+Progress_Label.pack()
 
 def main():
-
     console.print(f"[blue]Dandilion[/] Has Started at {datetime.now()}")
 
     console.print("[yellow]{}[/] Loaded".format(icon_relative_path))
     console.print("[yellow]{}[/] Loaded".format(theme_relative_path))
 
     App.mainloop()
-
 
 main()
